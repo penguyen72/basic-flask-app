204f10ce8c68087ad5330ae55b31c2c9558f72e2
penguyen <peytonn72@gmail.com>
2022-08-20 11:17:36 -0400

    create flash messages and database schema

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,18 @@
+import sqlite3
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     
     # This encrypts the cookies and session data related to website
     app.config["SECRET_KEY"] = 'super_secret_key'
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite3:///{DB_NAME}'
+
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
