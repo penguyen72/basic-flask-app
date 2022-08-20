c8f7ab65cf5817be850237d29ddf37ededa04f2d
penguyen <peytonn72@gmail.com>
2022-08-20 12:00:08 -0400

    add authentication functionality

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,8 @@
 import sqlite3
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -10,7 +12,7 @@ def create_app():
     
     # This encrypts the cookies and session data related to website
     app.config["SECRET_KEY"] = 'super_secret_key'
-    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite3:///{DB_NAME}'
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
 
     db.init_app(app)
 
@@ -20,4 +22,21 @@ def create_app():
     app.register_blueprint(views, url_prefix = "/")
     app.register_blueprint(auth, url_prefix = "/")
 
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")
